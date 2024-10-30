9e3c2a78dbe5ffc722460242020a38a5b710530e
Dhairya-A-Mehra <mehra.dhairya.btech2022@sitpune.edu.in>
2024-10-30 07:46:43 +0530

    admin table insertion query added in database

---
+++ database/admin_table_insertion.sql
@@ -0,0 +1,5 @@
+-- insertion in admin table this are dummy entries
+
+INSERT INTO admin (admin_id, admin_name, admin_email, admin_address, admin_phone, admin_password, date_of_joining) VALUES
+(1, 'Mark Taylor', 'mark.taylor@example.com', '321 Birch St, Springfield', '321-654-0987', 'hashed_password_a', '2024-08-01'),
+(2, 'Lucy White', 'lucy.white@example.com', '654 Cedar St, Springfield', '321-654-0988', 'hashed_password_b', '2024-08-10');
