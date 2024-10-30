f4a88c9773b743b9d72b1e23c09e9d4371a26c12
Dhairya-A-Mehra <mehra.dhairya.btech2022@sitpune.edu.in>
2024-10-30 07:52:57 +0530

    task table insertion query added in database

---
+++ database/task_table_insertion.sql
@@ -0,0 +1,7 @@
+-- task table insertion query, this are dummy entries
+
+INSERT INTO task (task_id, task_name, task_status, report_id) VALUES
+(1, 'Collect Garbage', 'Completed', 1),
+(2, 'Sort Food Items', 'Completed', 2),
+(3, 'Provide Check-Ups', 'Pending', 3),
+(4, 'Distribute Medicines', 'Completed', 3);
