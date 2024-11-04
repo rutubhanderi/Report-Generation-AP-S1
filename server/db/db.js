8d8c55e6d107f5d04d1d9cb1f33ac9609d57cc8b
Dhairya Mehra <mpdhairyamehra000942@gmail.com>
2024-11-04 19:08:04 +0530

    Removed Supabase API keys, and URL.

--- server/db/db.js
+++ server/db/db.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL || 'https://fvgktzbhhumqwyjwylqf.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2Z2t0emJoaHVtcXd5and5bHFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA2NDkzNTksImV4cCI6MjA0NjIyNTM1OX0.40Ee4bWx_y3yi8oD4QY6PDs4qpOYcB_OLmtyYAC4Ge0';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
