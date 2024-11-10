4f0b758a1ed5fec24dce6e8ab92b60259814e98e
Dhairya-A-Mehra <mehra.dhairya.btech2022@sitpune.edu.in>
2024-11-10 20:54:43 +0530

    setup according to render

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const supabase = require('./db/db');
+import path from 'path';
 
 const AdminRouter = require('./routes/admin.js');
 const VolunteerRouter = require('./routes/volunteer.js');
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -31,6 +34,11 @@ app.get('/', async (_request, _response) => {
 app.use('/admin', AdminRouter);
 app.use('/volunteer', VolunteerRouter);
 // app.use('/admin/volunteerlist', AdminRouter);
+
+app.use(express.static(path.join(__dirname, 'client/dev')));
+app.get('*', (_request, response) => {
+  response.sendFile(path.join(__dirname, 'client/dev', 'index.html'));
+});
 
 app.listen(PORT, ()=> {
   console.log(`listening at port ${PORT}`);
